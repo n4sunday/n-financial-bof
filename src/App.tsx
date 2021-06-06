2c8b066de747c06984a27bbec29f9fd49e5591ef
Sunday <nsoooooooooosn@gmail.com>
2021-06-07 01:50:26 +0700

    refactor: update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './modules/home'
 import Financial from './modules/financial'
 
 function App() {
   return (
-    <Router>
-      <div>
+    <BrowserRouter>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/financial" component={Financial} />
-      </div>
-    </Router>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
